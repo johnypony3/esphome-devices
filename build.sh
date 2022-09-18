6b777aa913f86854772fa726b3eaac4a88f81f2c
ponyboy3 <edward@theemm.com>
2022-09-18 13:24:29 -0700

    * updated build to work and readme about secrets

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-esphome compile $1.yaml
+DEVICE=$(basename ${1%.*})
+FULL_PATH=$1
+IP=$2
+echo "compiling:$DEVICE from $FULL_PATH"
 
-rm -rf  ~/Volumes/dev/firmwares/$1 2> /dev/null
-mkdir -p  ~/Volumes/dev/firmwares/$1
-
-cp ./builds/$1/.pioenvs/**/firmware.bin  ~/Volumes/dev/firmwares/$1/$1.bin
-cp .esphome/$1.yaml.json  ~/Volumes/dev/firmwares/$1/
-
-if [[ $2 ]]
+if [[ $IP ]]
 then
-    esphome upload $1.yaml --device $2
-else
-    esphome upload $1.yaml
+    echo "using ip:$IP"
 fi
 
-esphome clean $1.yaml
-esphome logs $1.yaml
+esphome compile $FULL_PATH
+
+rm -rf  ~/Volumes/dev/firmwares/$DEVICE 2> /dev/null
+mkdir -p  ~/Volumes/dev/firmwares/$DEVICE
+
+cp ../builds/$DEVICE/.pioenvs/**/firmware.bin  ~/Volumes/dev/firmwares/$DEVICE/$DEVICE.bin
+cp ..esphome/$FULL_PATH.json  ~/Volumes/dev/firmwares/$DEVICE/
+
+if [[ $IP ]]
+then
+    esphome upload $FULL_PATH --device $IP
+else
+    esphome upload $FULL_PATH
+fi
+
+esphome clean $FULL_PATH
+esphome logs $FULL_PATH
--- readme.md
+++ readme.md
@@ -20,3 +20,19 @@
 * copy an existing device file
 * modify as per your suit
 * run build.sh
+
+## notes
+
+* a secrets.yaml file is necessary in the root of the directory
+  * the current contents are:
+    * wifi_ssid
+    * wifi_password
+    * ota_password
+    * api_password
+    * ap_ppassword
+    * mqtt_password
+  * stored in 1pass
+    * op document get git_esphome_secret --vault personal --output secret.yaml
+  * i symlink the root dir to the defs and devices dirs
+    * ln -s $HOME/git/esphome/secrets.yaml $HOME/git/esphome/devices/ 
+    * ln -s $HOME/git/esphome/secrets.yaml $HOME/git/esphome/defs/ 
